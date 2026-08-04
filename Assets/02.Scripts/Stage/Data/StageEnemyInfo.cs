a1aa621c3d57bf774e3b0f789bda6a67139fe93d
NamHyunjae <skaguswoxx66@gmail.com>
2026-08-04 18:22:43 +0900

    docs : Updata StageEnemyInfo
    emey -> enemy

--- Assets/02.Scripts/Stage/Data/StageEnemyInfo.cs
+++ Assets/02.Scripts/Stage/Data/StageEnemyInfo.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class StageEnemyInfo 
 {
     [Header("등장할 적 프리팹")]
-    [SerializeField] private GameObject emeyPrefab;
+    [SerializeField] private GameObject enemyPrefab;
 
     [Header("적이 등장할 배치 위치")]
     [SerializeField] private EnemySpawnSlot spawnSlot;
 
-    public GameObject EmeyPrefab => emeyPrefab;
+    public GameObject EnemyPrefab => enemyPrefab;
     public EnemySpawnSlot SpawnSlot => spawnSlot;
 }
