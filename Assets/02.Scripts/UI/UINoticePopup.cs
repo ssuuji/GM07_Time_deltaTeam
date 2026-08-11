e5b54560aff2812ab03e05d65f7eb3cbc717d0b2
ssuuji <sub9399@naver.com>
2026-08-11 16:49:21 +0900

    feat : 영웅탭 파티 부분 UI 연결

---
+++ Assets/02.Scripts/UI/UINoticePopup.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace AFKHero.UI
+{
+    public class UINoticePopup : MonoBehaviour
+    {
+        public static UINoticePopup Instance { get; private set; }
+
+        [SerializeField] private GameObject noticePanel; //검정배경
+        [SerializeField] private TMP_Text noticeText;    //문구
+
+        private void Awake()
+        {
+            Instance = this;
+            noticePanel.SetActive(false);
+        }
+
+        //알림 표시
+        public void Show(string message)
+        {
+            noticeText.text = message;
+            noticePanel.SetActive(true);
+        }
+
+        //알림 숨기기
+        public void Hide()
+        {
+            noticePanel.SetActive(false);
+        }
+    }
+}
