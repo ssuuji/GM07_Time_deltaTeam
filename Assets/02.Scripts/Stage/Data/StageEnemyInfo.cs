556609f246c431fba3fe4adc21fcd79f10d93fa4
hcs8697-glitch <hcs8697@gmail.com>
2026-08-12 19:03:12 +0900

    feat : StageManager에 프로퍼티 추가, StageInfo 수정
    - StageManager에 현재 스테이지, 현재 섹션을 반환할 프로퍼티가 추가되었습니다.
    
    - StageInfo 기반 전투 테스트를 위해 StageInfo의 적 리스트가 StageEnemyInfo 클래스에서 UnitData로 임시변경되었습니다.
    
    - StageInfo 기반 전투 테스트를 위해 임의로 BattleSpawner에 메서드를 추가해 테스트해봤습니다. Discard Changes를 통해 변경사항 반영하지 않게 했는데 충돌 발생 시 말씀 부탁드립니다.
    [08/12 기록사항]
    
    BattleSpawner에 메서드 추가해봤었음
    
            public void RegisterEnemies(List<UnitData> enemies, int enemyLevel)
            {
                if(enemies == null)
                {
                    Debug.LogError("    Ʈ          ϵǾ        ʽ  ϴ .");
                    return;
                }
    
                enemyParty.Clear();
    
                for (int i = 0; i < enemies.Count; i++)
                {
                    if (enemies[i] == null) break;
    
                    enemyParty.Add(enemies[i]);
                }
    
            }
    
    StageInfo에서 StageEnemyInfo 대신 UnitData를 리스트로 설정했고, SO에셋 내부에 등록했었음.
    
    그리고, StageManager에서 BattleSpawner를 필드로 갖게 한 다음, 위에서 만든 메서드를 실행했었고 추가로 SpawnAllUnits도 호출했었음.
    
            battleSpawner.RegisterEnemies(currentStageInfo.Enemies, enemyLevel);
            battleSpawner.SpawnAllUnits();
    
    적을 처치 시 다음 스테이지로 정상적으로 넘어갔으나, 간헐적으로 전투하지 않는 문제 발생.

--- Assets/02.Scripts/Stage/Data/StageEnemyInfo.cs
+++ Assets/02.Scripts/Stage/Data/StageEnemyInfo.cs
@@ -1,5 +1,21 @@
-﻿using System;
+﻿using AFKHero.Battle;
+using System;
 using UnityEngine;
+
+//해당 스테이지에서 소환할 적의 목록을 담는 컨테이너형 클래스.
+//소환 위치는 이곳에서 결정하지 말고, 소환할 곳에서 담당한다.
+
+
+//근데, 생각해보니까 어차피 이 클래스는 StageInfo에서 List로 관리된다.
+//이미 구현된 UnitData같은 다른 클래스를 StageInfo가 List로 필드로 가지면 되는 거 아닌가?
+
+//StageInfo 스크립트에서, List<StageEnemyInfo>를 다른 걸로 교체해버리면, 이 클래스 자체가 필요가 없어진다.
+
+//그런데, 엥간하면 영웅 쪽에서 만든 Data를 사용하는 방식으로 BattleSpawner, StageInfo를 수정하는 게 맞기 때문에
+//UnitData를 활용하는 방안은 BattleSpawner가 동작하기 위한 임시방편일 뿐이다.
+
+//결론적으로, 영웅 쪽 데이터와 전투시스템 쪽 데이터 중 어느 쪽을 사용해야 할 지 결정해야 한다.
+
 
 [Serializable]
 public class StageEnemyInfo 
@@ -10,6 +26,9 @@ public class StageEnemyInfo
     [Header("적이 등장할 배치 위치")]
     [SerializeField] private EnemySpawnSlot spawnSlot;
 
+    [SerializeField] private UnitData unitData;
+
     public GameObject EnemyPrefab => enemyPrefab;
     public EnemySpawnSlot SpawnSlot => spawnSlot;
+    public UnitData UnitData => unitData;
 }
