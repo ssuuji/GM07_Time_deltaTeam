7bea76503d6161ff7eaa4e4b8e03435a691f443e
ssuuji <sub9399@naver.com>
2026-08-17 16:57:09 +0900

    빌드관련 셋팅 추가

--- Assets/02.Scripts/Hero/Data/HeroData.cs
+++ Assets/02.Scripts/Hero/Data/HeroData.cs
@@ -1,5 +1,4 @@
-﻿using Mono.Cecil.Cil;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewHeroData", menuName = "Game/Hero/Hero Data")]
 public class HeroData : ScriptableObject
