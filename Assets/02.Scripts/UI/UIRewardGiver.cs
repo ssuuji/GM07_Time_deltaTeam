7a38a98dd5e442e866297a5c3ecf52947abc70fb
hcs8697-glitch <hcs8697@gmail.com>
2026-09-06 22:50:51 +0900

    feat : 게임씬 수정, 승리 / 패배 패널에 효과 및 효과음 재생 추가
    - 패널이 활성화되면 효과와 효과음을 재생하고, 비활성화되면 초기화하도록 하는 스크립트 PanelShowEffector 클래스를 생성했습니다.
    
    - Game씬의 승리패널과 패배패널에 컴포넌트로 추가하였고, 의도대로 동작하는 것을 확인했습니다.
    
    - Enums의 SoundKey 열거형을 수정하여 승리 효과음, 패배 효과음 상수를 추가했습니다.
    
    - Sound 프리팹에 승리 효과음, 패배 효과음을 할당했습니다.
    
    - 호출자가 자신의 위치와 갯수, 보상 타입을 건네면 목표한 UI로 이미지를 생성해 보내는 클래스 UIRewardGiver를 UIQuestManager를 바탕으로 작성해보려 했으나 실패했습니다. 전부 주석으로 처리하여 의도치않게 호출하지 않도록 했습니다.

---
+++ Assets/02.Scripts/UI/UIRewardGiver.cs
@@ -0,0 +1,85 @@
+﻿//using AFKHero.Quest;
+//using DG.Tweening;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+////보상이 발생했을 때 호출하여, 정해진 위치로 정해진 이미지를 이동시키고 삭제할 클래스
+////보내는 위치는 얘만 알면 되고, 시작 위치와 재화를 특정 클래스에서 전달해야 한다.
+
+//public class UIRewardGiver : MonoBehaviour
+//{
+//    public static UIRewardGiver Instance { get; private set; }
+
+//    [Header("보상 이미지")]
+//    [SerializeField] private GameObject goldImage;
+//    [SerializeField] private GameObject diaImage;
+//    [SerializeField] private GameObject ticketImage;
+//    [SerializeField] private GameObject weaponImage;
+
+//    [Header("보낼 위치")]
+//    [SerializeField] private RectTransform goldUI;
+//    [SerializeField] private RectTransform diaUI;
+//    [SerializeField] private RectTransform ticketUI;
+//    [SerializeField] private RectTransform weaponUI;
+
+
+//    private Sequence seq;
+
+
+
+//    private void Awake()
+//    {
+//        if(Instance == null)
+//        {
+//            Instance = this;
+//        }
+//        else
+//        {
+//            Destroy(gameObject);
+//        }
+//    }
+
+//    public void PlayRewardEffect(RewardType reward, Vector3 position)
+//    {
+//        GameObject rewardImage = null;
+//        RectTransform rewardTarget = null;
+
+//        switch (reward)
+//        {
+//            case RewardType.Gold:
+//                rewardImage = goldImage;
+//                rewardTarget = goldUI;
+//                break;
+//            case RewardType.Dia:
+//                rewardImage = diaImage;
+//                rewardTarget = diaUI;
+//                break;
+//            case RewardType.FreeTicket:
+//                rewardImage = ticketImage;
+//                rewardTarget = ticketUI;
+//                break;
+//            default:
+//                break;
+//        }
+
+//        if(rewardImage == null || rewardTarget == null)
+//        {
+//            return;
+//        }
+
+
+
+//    }
+
+//    private void PlayRewardEffect(GameObject rewardImage, RectTransform rewardTarget, Vector3 position)
+//    {
+  
+//        Vector2 targetPosition = rewardTarget.anchoredPosition;
+
+//        GameObject rewardEffect = Instantiate(rewardImage, position, Quaternion.identity);
+
+
+//    }
+
+
+//}
